bad088e4ff66efc195776ae039a85efe327ce893
StjepanMat <stjepan.mateljan@gmail.com>
2023-11-14 14:18:47 +0100

    Tasks #1,#2, #3 completed (database created)

---
+++ Ispit.Data/Migrations/20231114130620_initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ispit.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
---
+++ Ispit.Data/Models/TodoListContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ispit.Data.Models
+{
+    public partial class TodoListContext:DbContext
+    {
+        public TodoListContext()
+        {
+        }
+
+        public TodoListContext(DbContextOptions<TodoListContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<TodoList> TodoList { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=parcijalni_11_API;" +
+                    "integrated security=true;MultipleActiveResultSets=true;TrustServerCertificate=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoList>( entity =>
+            {
+                entity.Property(e=> e.Id)
+                .ValueGeneratedNever();
+
+                entity.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+                entity.Property(e => e.Description)
+                .HasMaxLength(300);
+
+                entity.Property(e=> e.IsCompleted)
+                .IsRequired();
+
+            }
+
+                );
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
